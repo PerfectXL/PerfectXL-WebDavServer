dc77a0c789015b851d0b1c356aba0c451f40ea60
Roel van der Steen <roel@st2x.net>
2019-02-05 09:48:03 +0100

    Ensure data directory

--- WebDavService.cs
+++ WebDavService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using NLog;
@@ -22,13 +23,13 @@ namespace PerfectXL.WebDavServer
             MyLogger.Info($"Stop {nameof(WebDavService)}");
         }
 
-        private static string Hash(string password)
-        {
-            return Convert.ToBase64String(new SHA256CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(password)));
-        }
-
         private static void StartServer()
         {
+            if (!EnsureDataDirectory())
+            {
+                return;
+            }
+
             IWebDavStore store = new WebDavDiskStore(AppSettings.FilePath);
             MyLogger.Debug($"{nameof(IWebDavStore)} @ {AppSettings.FilePath}");
             var listenerAdapter = new BasicAuthHttpListenerAdapter();
@@ -40,5 +41,32 @@ namespace PerfectXL.WebDavServer
             MyLogger.Debug($"Listening on {url}");
             server.Start(url);
         }
+
+        #region Helpers
+        private static bool EnsureDataDirectory()
+        {
+            try
+            {
+                Directory.CreateDirectory(AppSettings.FilePath);
+            }
+            catch (Exception e)
+            {
+                MyLogger.Fatal(e, $"Directory.CreateDirectory({AppSettings.FilePath}) failed. {e.Message}");
+            }
+
+            if (Directory.Exists(AppSettings.FilePath))
+            {
+                return true;
+            }
+
+            MyLogger.Fatal($"Directory {AppSettings.FilePath} does not exist. Abort.");
+            return false;
+        }
+
+        internal static string Hash(string password)
+        {
+            return Convert.ToBase64String(new SHA256CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(password)));
+        }
+        #endregion
     }
 }
