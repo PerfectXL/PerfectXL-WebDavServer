4f10cd491617e7523cbee8ea879dccc360f5427d
Roel van der Steen <roel@st2x.net>
2019-02-06 10:09:58 +0100

    Update WebDAV NuGet package
    Application version is now 0.1.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7a312385-a2f0-4c71-9b7d-a22ab0aa7a00")]
-[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
